f282e141052193b56085bcf1af0c77ad97556dd9
Abbosbeck <Abbosalisherovich2003@gmail.com>
2025-02-26 20:00:52 +0500

    CORE: Add Blog Service

---
+++ PersonalBlog.Core/Services/IBlogService.cs
@@ -0,0 +1,12 @@
+﻿using PersonalBlog.Core.ViewModels;
+
+namespace PersonalBlog.Core.Services
+{
+    public interface IBlogService
+    {
+        Task<string> AddBlogAsync(BlogCreateAndUpdateViewModel blogCreateViewModel);
+        Task<IEnumerable<BlogTitleViewModel>> GetBlogTitlesAsync();
+        Task<BlogViewModel> GetBlogBySlugAsync(string slug);
+        Task UpdateBlogAsync(string slug, BlogCreateAndUpdateViewModel blogUpdateViewModel);
+    }
+}
