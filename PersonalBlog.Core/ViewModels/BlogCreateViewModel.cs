65541392004a392d19e0eaf134fb4a218914574f
Abbosbeck <Abbosalisherovich2003@gmail.com>
2025-02-26 19:19:53 +0500

    CORE: Delete Uncessary Parts of Code And Add Explicit Casting

--- PersonalBlog.Core/ViewModels/BlogCreateViewModel.cs
+++ PersonalBlog.Core/ViewModels/BlogCreateViewModel.cs
@@ -1,11 +1,21 @@
-﻿namespace PersonalBlog.Core.ViewModels
+﻿using PersonalBlog.Data.Entities;
+
+namespace PersonalBlog.Core.ViewModels
 {
     public class BlogCreateViewModel
     {
         public string Title { get; set; }
         public string Content { get; set; }
-        public Guid AuthorId { get; set; }
         public bool IsPublished { get; set; }
-        public DateOnly PublishedAt { get; set; }
+
+        public static explicit operator BlogEntity(BlogCreateViewModel blogCreateViewModel)
+        {
+            return new BlogEntity
+            {
+                Title = blogCreateViewModel.Title,
+                Content = blogCreateViewModel.Content,
+                IsPublished = blogCreateViewModel.IsPublished
+            };
+        }
     }
-}
+}    
