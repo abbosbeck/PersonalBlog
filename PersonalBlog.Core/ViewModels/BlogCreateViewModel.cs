ab9b543b5aa68a7dc6232860aa670e87bb2eeec5
Abbosbeck <Abbosalisherovich2003@gmail.com>
2025-02-26 19:08:55 +0500

    CORE: Add Blog Create View Model

---
+++ PersonalBlog.Core/ViewModels/BlogCreateViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace PersonalBlog.Core.ViewModels
+{
+    public class BlogCreateViewModel
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public Guid AuthorId { get; set; }
+        public bool IsPublished { get; set; }
+        public DateOnly PublishedAt { get; set; }
+    }
+}
