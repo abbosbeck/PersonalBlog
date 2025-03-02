d094b52177b118b4b42e5f9fc480f461323fbdce
Abbosbeck <Abbosalisherovich2003@gmail.com>
2025-03-02 13:29:21 +0500

    DATA: Add Blog Images Entity and Inject it in Blog Entity

---
+++ PersonalBlog.Data/Entities/BlogImageEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace PersonalBlog.Data.Entities
+{
+    public class BlogImageEntity
+    {
+        public Guid Id { get; set; }
+        public string Url { get; set; }
+        public Guid BlogId { get; set; }
+        public BlogEntity Blog { get; set; }
+    }
+}
