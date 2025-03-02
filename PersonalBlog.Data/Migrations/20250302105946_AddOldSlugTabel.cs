0165555db2f8d5442760b34a113c5e682ed87a67
Abbosbeck <Abbosalisherovich2003@gmail.com>
2025-03-02 16:01:48 +0500

    DATA: Add Migration

---
+++ PersonalBlog.Data/Migrations/20250302105946_AddOldSlugTabel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PersonalBlog.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddOldSlugTabel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "OldSlugs",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    OldSlug = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    BlogId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OldSlugs", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_OldSlugs_Blogs_BlogId",
+                        column: x => x.BlogId,
+                        principalTable: "Blogs",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OldSlugs_BlogId",
+                table: "OldSlugs",
+                column: "BlogId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "OldSlugs");
+        }
+    }
+}
