48091973b3630df6bc781d2ee6e27b1d12da4b62
Abbosbeck <Abbosalisherovich2003@gmail.com>
2025-03-02 15:36:22 +0500

    API: Add Image Controller

---
+++ PersonalBlog.Api/Controllers/BlogImageController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PersonalBlog.Core.Services;
+
+namespace PersonalBlog.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogImageController : ControllerBase
+    {
+        private readonly IImageSerivce _imageService;
+        public BlogImageController(IImageSerivce imageSerivce)
+        {
+            _imageService = imageSerivce;
+        }
+
+        [HttpPost("Add Image")]
+        public async Task<IActionResult> AddImageAsync(IFormFile imageFile, [FromForm] string blogSlug)
+        {
+            var url = await _imageService.SaveImageAsync(imageFile, blogSlug);
+         
+            return Ok(url);
+        }
+        [HttpPost("Add Images")]
+        public async Task<IActionResult> AddImagesAsync(IEnumerable<IFormFile> imageFiles, [FromForm] string blogSlug)
+        {
+            var urls = await _imageService.SaveImagesAsync(imageFiles, blogSlug);
+
+            return Ok(urls);
+        }
+    }
+}
