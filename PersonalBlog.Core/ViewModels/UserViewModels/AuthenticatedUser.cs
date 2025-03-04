e36c7e0b074e431b452930898ce2830a6a03a302
Abbosbeck <Abbosalisherovich2003@gmail.com>
2025-03-04 12:20:30 +0500

    CORE: Add Authenticated User View Model

---
+++ PersonalBlog.Core/ViewModels/UserViewModels/AuthenticatedUser.cs
@@ -0,0 +1,12 @@
+﻿namespace PersonalBlog.Core.ViewModels.UserViewModels
+{
+    class AuthenticatedUser
+    {
+        public string Name { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string GitHub { get; set; }
+        public string LinkedIn { get; set; }
+        public string Telegram { get; set; }
+    }
+}
