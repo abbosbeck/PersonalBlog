2a2266ba3ce04fe0998e2461d9bd2bdfd95d2dcf
Abbosbeck <Abbosalisherovich2003@gmail.com>
2025-03-04 12:30:03 +0500

    CORE: Add User Register View Model

---
+++ PersonalBlog.Core/ViewModels/UserViewModels/UserRegisterViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace PersonalBlog.Core.ViewModels.UserViewModels
+{
+    public class UserRegisterViewModel
+    {
+        public string Name { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string GitHub { get; set; }
+        public string LinkedIn { get; set; }
+        public string Telegram { get; set; }
+    }
+}
