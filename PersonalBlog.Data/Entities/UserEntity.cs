fde6048fb4ff8095ed6e693b250ef74b1b59c52d
Abbosbeck <Abbosalisherovich2003@gmail.com>
2025-03-04 12:07:20 +0500

    DATA: Create User Entity

---
+++ PersonalBlog.Data/Entities/UserEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace PersonalBlog.Data.Entities
+{
+    public class UserEntity
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string GitHub { get; set; }
+        public string LinkedIn { get; set; }
+        public string Telegram { get; set; }
+    }
+}
