4cc9d2bcc6f1c6dd4e308ddccf4f5b67ddf24662
Abbosbeck <Abbosalisherovich2003@gmail.com>
2025-03-04 12:31:36 +0500

    CORE: Add IUserSerivce

---
+++ PersonalBlog.Core/Services/UserServices/IUserService.cs
@@ -0,0 +1,11 @@
+﻿using PersonalBlog.Core.ViewModels.UserViewModels;
+
+namespace PersonalBlog.Core.Services.UserServices
+{
+    public interface IUserService
+    {
+        Task<AuthenticatedUser> AuthenticateAsync(UserLoginViewModel userLoginViewModel);
+        Task<AuthenticatedUser> RegisterAsync(UserRegisterViewModel userRegisterViewModel);
+        Task<AuthenticatedUser> UpdateUserDataAsyn(AuthenticatedUser authenticatedUser);
+    }
+}
