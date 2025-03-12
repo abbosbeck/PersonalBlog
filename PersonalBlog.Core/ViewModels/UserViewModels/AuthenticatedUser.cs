b315ae5c1a25b0352e56d9b314aa845a03716165
Abbosbeck <Abbosalisherovich2003@gmail.com>
2025-03-12 17:06:09 +0500

    CORE: Make Authenticated User's Casting Form

--- PersonalBlog.Core/ViewModels/UserViewModels/AuthenticatedUser.cs
+++ PersonalBlog.Core/ViewModels/UserViewModels/AuthenticatedUser.cs
@@ -1,12 +1,27 @@
-﻿namespace PersonalBlog.Core.ViewModels.UserViewModels
+﻿using PersonalBlog.Data.Entities;
+
+namespace PersonalBlog.Core.ViewModels.UserViewModels
 {
     public class AuthenticatedUser
     {
         public string Name { get; set; }
-        public string UserName { get; set; }
+        public string Username { get; set; }
         public string Password { get; set; }
         public string GitHub { get; set; }
         public string LinkedIn { get; set; }
         public string Telegram { get; set; }
+
+        public static explicit operator AuthenticatedUser(UserEntity user)
+        {
+            return new AuthenticatedUser
+            {
+                Name = user.Name,
+                Username = user.Username,
+                Password = user.Password,
+                GitHub = user.GitHub,
+                LinkedIn = user.LinkedIn,
+                Telegram = user.Telegram,
+            };
+        }
     }
 }
